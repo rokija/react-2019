c765da977cde2e17e7fdf4b09004057c92a647cc
Ruta <rutafreimane14@gmail.com>
2019-02-12 11:46:29 +0200

    created PostModal component

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends Component {
             </div>
           </Row>
         </Container>
+        {/* <PostModal /> */}
       </div>
     );
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
 import { Button } from "reactstrap";
+import PostModal from "../PostModal/PostModal";
 import accentureLogo from "../../assets/logo-acn.png";
 import "./Header.css";
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  onOpenModal = () => {
+    this.setState({ isOpen: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ isOpen: false });
+  };
+
   render() {
+    const { isOpen } = this.state;
+
     return (
       <div className="Header">
         <div className="Header__title">
@@ -15,7 +34,10 @@ class Header extends Component {
           />
           Bootcamp title
         </div>
-        <Button color="dark">Upload</Button>
+        <Button onClick={this.onOpenModal} color="dark">
+          Upload
+        </Button>
+        {isOpen && <PostModal onClose={this.onCloseModal} />}
       </div>
     );
   }
